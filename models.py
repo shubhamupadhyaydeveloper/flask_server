83a7a448ffae0506ec4b05efd6c4d994714259a2
shubham upadhyay <shubhamwork48@gmail.com>
2024-10-03 23:25:32 +0530

    create and get routes

--- models.py
+++ models.py
@@ -3,9 +3,16 @@ from server import db
 class Friend(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(100),nullable=False)
+    gender = db.Column(db.String(20),nullable=False)
+    age = db.Column(db.Integer,nullable=False)
+    image_url = db.Column(db.String,nullable=False)
+    
 
     def to_json(self):
         return {
             "id" : self.id,
-            "name" : self.name
+            "name" : self.name,
+            "gender" : self.gender,
+            "age" : self.age,
+            "imageUrl" : self.image_url
         }
--- route.py
+++ route.py
@@ -1,4 +1,4 @@
-from server import app
+from server import app,db
 from models import Friend
 from flask import request,jsonify
 
@@ -18,3 +18,25 @@ def user_name(username):
     age = request.json['age']
     return f"this is your name {username} and this is your age {age}"
 
+@app.route("/api/save",methods=["POST"])
+def register_friend():
+    data = request.json
+    name = data.get('name')
+    gender = data.get('gender')
+    age = data.get('age')
+    image_url = ''
+
+    if gender == 'male':
+        image_url = f'https://avatar.iran.liara.run/public/boy?username={name}'
+    elif gender == 'female':
+        image_url = f'https://avatar.iran.liara.run/public/girl?username={name}'
+        
+    try:
+      new_friend = Friend(name=name,gender=gender,age=age,image_url=image_url)
+      db.session.add(new_friend)
+      db.session.commit()
+
+      return jsonify({"message" : "user created successul","friend" : new_friend.to_json()}),201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error" : str(e)}),500
